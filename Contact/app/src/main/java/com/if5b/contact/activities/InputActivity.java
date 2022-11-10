226b72fbe069114c449f8a3abea148791cf7cc4a
Dmas <100113789+MAzrilFahrezi@users.noreply.github.com>
2022-11-10 15:11:48 +0700

    Contact(Room)

---
+++ Contact/app/src/main/java/com/if5b/contact/activities/InputActivity.java
@@ -0,0 +1,17 @@
+package com.if5b.contact.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.if5b.contact.R;
+import com.if5b.contact.databinding.ActivityInputBinding;
+
+public class InputActivity extends AppCompatActivity {
+    private ActivityInputBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityInputBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
